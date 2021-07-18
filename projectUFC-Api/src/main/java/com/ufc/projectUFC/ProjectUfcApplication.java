7c9a29adcdd6ecc4fe75609b18a6653cf74f7293
willianpilar <willianpilar@hotmail.com>
2021-07-17 22:23:50 -0300

    Adicionando API UFC - Padrão de projeto

---
+++ projectUFC-Api/src/main/java/com/ufc/projectUFC/ProjectUfcApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.projectUFC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUfcApplication.class, args);
+	}
+
+}
---
+++ projectUFC-Api/src/test/java/com/ufc/projectUFC/ProjectUfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.projectUFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
